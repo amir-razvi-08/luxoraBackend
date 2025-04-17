9a676c33ace38fcc79c0c837be5bc4f330d200dc
Amir Razvi <amirrazvi8@gmail.com>
2025-04-17 21:51:15 +0530

    fix: update cookie security options for CORS compliance

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const DB_NAME = "ecommerce"
 export const options = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "Strict",
+  secure: true,
+  sameSite: "None",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 ;
